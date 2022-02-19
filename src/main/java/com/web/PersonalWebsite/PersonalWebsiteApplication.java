2d70e59cc2b607843b1ed179b797732ef2afec58
Michael <kramermp5@gmail.com>
2022-02-19 13:52:46 -0500

    Initialize

---
+++ src/main/java/com/web/PersonalWebsite/PersonalWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.web.PersonalWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class PersonalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteApplication.class, args);
+	}
+
+}
